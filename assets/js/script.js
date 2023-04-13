26d326b32be38e5e1a24606b2c04ff7cc0dd84b2
mirkettinho <mirkodifranco30@gmail.com>
2023-04-13 17:35:21 +0200

    [add] thumbs

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,14 @@ createApp({
         "./assets/img/supra.jpg",
         "./assets/img/porsche.jpg"
       ],
+      thumbImage:[
+        "./assets/img/ferrari.jpg",
+        "./assets/img/gtr.jpg",
+        "./assets/img/r8.jpg",
+        "./assets/img/silver.jpg",
+        "./assets/img/supra.jpg",
+        "./assets/img/porsche.jpg"
+      ],
        counter: 0,
        loading: true
 
@@ -24,12 +32,17 @@ createApp({
 
       if(this.counter === this.image.length) this.counter = 0
       if(this.counter < 0) this.counter = this.image.length -1
-    }
+    },
+    autoplay{
+    setInterval(()=>{
+      skip(true)
+    },2000)
+  }
   },
 
   mounted(){
     setTimeout(()=>{
       this.loading = false
-    },1000)
+    },2000)
   }
 }).mount("#app")
