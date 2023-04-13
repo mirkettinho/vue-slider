5d9d2023482549bbd19bd5119cdd9e8ec1eccb2b
mirkettinho <mirkodifranco30@gmail.com>
2023-04-13 16:49:52 +0200

    [add] img + fix slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,33 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      image: "./assets/img/ferrari.jpg",
-      counter: 0
+      image:[
+        "./assets/img/ferrari.jpg",
+        "./assets/img/gtr.jpg",
+        "./assets/img/r8.jpg",
+        "./assets/img/silver.jpg",
+        "./assets/img/supra.jpg",
+        "./assets/img/porsche.jpg"
+      ],
+       counter: 0,
+       loading: true
+
     }
+  },
+
+  methods:{
+    skip(isNext){
+      if(isNext) this.counter++
+      else this.counter--
+
+      if(this.counter === this.image.length) this.counter = 0
+      if(this.counter < 0) this.counter = this.image.length -1
+    }
+  },
+
+  mounted(){
+    setTimeout(()=>{
+      this.loading = false
+    },1000)
   }
 }).mount("#app")
